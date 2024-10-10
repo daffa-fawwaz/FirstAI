fe38f98890028dc272d69e4692e30082c6dc7fe1
DaffaFawwaz <143053942+daffa-fawwaz@users.noreply.github.com>
2024-10-10 10:09:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,25 @@ async function sendReq() {
        } 
        load.classList.add("hidden")
        const data = await response.json();
-       answer.innerHTML = data.result;
-       console.log(data);
+       var htmlContent = renderApiResult(data.result)
+       answer.innerHTML = htmlContent;
+    //    console.log(data);
+       answer.innerHTML = htmlContent;
     } catch (error) {
         console.log(error.message)
     }
+}
+
+
+function renderApiResult(result) {
+  // Pisahkan teks berdasarkan blok kode dan teks lainnya
+  let formattedHtml = result
+    .replace(/```html([^`]+)```/g, "<pre><code>$1</code></pre>") // Mengubah blok kode menjadi <pre><code>
+    .replace(/### (.+)/g, "<h3>$1</h3>") // Mengubah heading menjadi <h3>
+    .replace(/\n/g, "<br>") 
+    .replace(/\- (.+)/g, "<li>$1</li>") // Mengganti bullet point dengan <li>
+    .replace(/(\n\n)/g, "</p><p>") // Memisahkan paragraf
+    .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+  // Tambahkan wrapper <p> di awal dan akhir
+  return `<p>${formattedHtml}</p>`;
 }
